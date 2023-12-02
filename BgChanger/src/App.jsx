4fa6cef1001b37af9c741e4a7f0d389058e127da
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-02 14:14:05 +0530

    Final Update

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -11,22 +11,69 @@ function App() {
       >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
           <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-3 rounded-3xl">
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("green")}style={{backgroundColor: "green"}}>Green</button>
-            
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("blue")}style={{backgroundColor: "Blue"}}>Blue</button>
-            
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("indigo")}style={{backgroundColor: "Indigo"}}>Indigo</button>
-            
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("Violet")}style={{backgroundColor: "Violet"}}>Violet</button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("green")}
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
 
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("Yellowgreen")}style={{backgroundColor: "Yellowgreen"}}>Yellow-green</button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("blue")}
+              style={{ backgroundColor: "Blue" }}
+            >
+              Blue
+            </button>
 
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("red")}style={{backgroundColor: "Red"}}>Red</button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("indigo")}
+              style={{ backgroundColor: "Indigo" }}
+            >
+              Indigo
+            </button>
 
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("purple")}style={{backgroundColor: "Purple"}}>Purple</button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("Violet")}
+              style={{ backgroundColor: "Violet" }}
+            >
+              Violet
+            </button>
 
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={() => setCount("black")}style={{backgroundColor: "black"}}>black</button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("Yellowgreen")}
+              style={{ backgroundColor: "Yellowgreen" }}
+            >
+              Yellow-green
+            </button>
 
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("red")}
+              style={{ backgroundColor: "Red" }}
+            >
+              Red
+            </button>
+
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("purple")}
+              style={{ backgroundColor: "Purple" }}
+            >
+              Purple
+            </button>
+
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              onClick={() => setCount("black")}
+              style={{ backgroundColor: "black" }}
+            >
+              black
+            </button>
           </div>
         </div>
       </div>
