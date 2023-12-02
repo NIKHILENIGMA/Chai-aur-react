e2305a219bb031a8c8130b56da455812c7c5b822
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-02 12:52:26 +0530

    Update to Bgchanger

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import BackGround from "../components/background";
+
 
 // import "./App.css";
 
